00c109cf104bd442efb55ea7125ffddba2f0dc09
Cody Laminack <cody.laminack@gmail.com>
2016-06-24 13:03:25 -0400

    Figured out df melt

--- wide_receivers/wr_melt.py
+++ wide_receivers/wr_melt.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('cleaned_wrs.csv')
+df = pd.read_csv('wrs_finalish.csv')
 
 df.drop(['Unnamed: 0', 'Unnamed: 0.1'], axis = 1, inplace = True)
 
@@ -9,24 +9,9 @@ df.reset_index(inplace=True, drop=True)
 df.columns
 
 
-values = ['name','age', 'team', 'weight', 'games', 'targets', 'first_down_ctchs', 'receptions', 'yac']
+values = ['name','age', 'team', 'weight', 'games', 'targets', 'first_down_ctchs', 'receptions', 'yac', 'team']
 
-df2 = pd.pivot_table(df[df.season>=2014], values=values, index='name', columns = 'season')
-df2.head(50)
 
-data = df[df.season == 2015]
-
-### Trying to see how well you could impute YAC and other fox_receiving stats
-
-data[data.yac <= 5][values]
-
-test = data[data.yac == 0]
-test.shape
-
-train = data[data.yac != 0]
-
-train.columns
-features = ['targets', 'receptions', 'rec_yards', 'rec_tds', 'rec_ypg', 'pct_of_team_passyards', 'dpi_yards', 'height_inches']
 
 
 # Define a function that splits a pivoted dataframe into its training and target set
@@ -35,6 +20,18 @@ df[cols].head(15)
 
 # This will create a dataframe that has all the players who were rookies three seasons ago, and are therefore entering their fourth seasons.
 x = df[((df.years_in_league == 0)&(df.season==2013))|((df.years_in_league==1)&(df.season==2014))|((df.years_in_league==2)&(df.season==2015))|((df.years_in_league==3)&(df.season==2016))]
-x.head(25)
 
-x[x.name == 'DeAndre Hopkins']
+
+cols = [ 'season', 'age', 'team', 'yac', 'receptions']
+df2 = df[(df.years_in_league>=0)&(df.years_in_league<=3)]
+
+df2.shape
+
+df3 = df[(df.rookie_season <2013)& (df.years_in_league<=3)]
+df3.head()
+pivoted = df3.pivot_table(index=df3.name, columns='years_in_league', values=cols)
+
+
+
+
+pivoted.shape
